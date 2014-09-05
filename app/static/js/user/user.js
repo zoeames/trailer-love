824a1f1d1fbb59824f2cf89d32102b23e6b4e50a
Matt Keef <mkeef1@gmail.com>
2014-09-05 15:24:44 -0500

    map on index added

--- app/static/js/user/user.js
+++ app/static/js/user/user.js
@@ -4,7 +4,7 @@
   'use strict';
   var map;
   $(document).ready(function(){
-    initMap(0, 0, 2);
+    initMap(36.2, -85.7, 7);
     var positions = getPositions();
     positions.forEach(function(pos){
       addMarker(pos.lat, pos.lng, pos.name);
@@ -22,11 +22,12 @@
     return positions;
   }
   function initMap(lat, lng, zoom){
-    var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
+    var styles = [{'featureType':'water','stylers':[{'color':'#46bcec'},{'visibility':'on'}]},{'featureType':'landscape','stylers':[{'color':'#f2f2f2'}]},{'featureType':'road','stylers':[{'saturation':-100},{'lightness':45}]},{'featureType':'road.highway','stylers':[{'visibility':'simplified'}]},{'featureType':'road.arterial','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'labels.text.fill','stylers':[{'color':'#444444'}]},{'featureType':'transit','stylers':[{'visibility':'off'}]},{'featureType':'poi','stylers':[{'visibility':'off'}]}],
+    mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles:styles};
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
   function addMarker(lat, lng, name){
     var latLng = new google.maps.LatLng(lat, lng);
-    new google.maps.Marker({map: map, position: latLng, title: name, animation: google.maps.Animation.DROP});
+    new google.maps.Marker({map: map, position: latLng, title: name, animation: google.maps.Animation.DROP, icon: 'http://cdn.iconsmash.com/Content/icons/Simpsons-Springfield-V09/iconpreviews/32/Older%20Homer.png'});
   }
 })();
